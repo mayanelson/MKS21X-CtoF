105df612be4a34ca08bdb8063476aff20c1ef26e
Maya Nelson <mnelson30@stuy.edu>
2021-09-23 21:17:16 -0400

    Modified CtoFTester

---
+++ CtoFTester.java
@@ -0,0 +1,10 @@
+public class CtoFTester {
+
+public static double celciusToFahrenheit(double celcius) {
+  return (celcius * 9/5) + 32;
+}
+
+public static void main(String[] args) {
+  System.out.println(celciusToFahrenheit(20));
+}
+}
