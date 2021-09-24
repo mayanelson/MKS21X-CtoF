70b57a4f8161c8f6afebad1004ba0d6b82d75d20
Maya Nelson <mnelson30@stuy.edu>
2021-09-23 22:03:22 -0400

    Idk what i'm doing

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,10 +1,14 @@
 public class CtoFTester {
-
+// Function should have celcius and fahrenheit values as parameters, and these should be doubles.
 public static double celciusToFahrenheit(double celcius) {
   return (celcius * 9/5) + 32;
 }
+public static double fahrenheitToCelcius(double fahrenheit){
+  return (fahrenheit -32) * 5/9;
+}
 
 public static void main(String[] args) {
-  System.out.println(celciusToFahrenheit(20));
+  System.out.println(celciusToFahrenheit(20)); //should return 68
+  System.out.println(fahrenheitToCelcius(20)); //should return -6.666 repeating
 }
 }
